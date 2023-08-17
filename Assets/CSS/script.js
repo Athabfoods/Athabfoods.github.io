9044176694d5fd27779f74347a7c44464a1c5b3f
athabfoods@gmail.com <athabfoods@gmail.com>
2023-08-16 23:33:33 -0600

    assets

--- Assets/CSS/script.js
+++ Assets/CSS/script.js
@@ -9,15 +9,6 @@ function myFunction() {
   }
 }
 
-  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
@@ -36,4 +27,14 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
